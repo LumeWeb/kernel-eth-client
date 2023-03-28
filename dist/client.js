ad3397d8dcc9630ab60e49010da806ad9f848cad
Derrick Hammer <derrick@derrickhammer.com>
2023-03-28 07:57:05 -0400

    *Update dist

--- dist/client.js
+++ dist/client.js
@@ -1,5 +1,5 @@
 import { Client, factory } from "@lumeweb/libkernel-universal";
-const RPC_MODULE = "AAC1ZoCCdfa-mc7HwirhSF5ysMZNRUax-E7rFpEPOzh1Bg";
+const RPC_MODULE = "_B0BvJGSbPWH2tDbHpmhL_i7VpYXDGKzTL7gFLb0qtP5Hg";
 export class EthClient extends Client {
     async ready() {
         return this.callModuleReturn("ready");
