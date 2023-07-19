2a7eb58354839bfdea96504f00b17f0841dae869
Derrick Hammer <derrick@derrickhammer.com>
2023-07-19 12:50:45 -0400

    feat: add register for network registry

--- src/client.ts
+++ src/client.ts
@@ -9,6 +9,9 @@ export class EthClient extends Client {
   public async method(method: string, params: any) {
     return this.callModuleReturn(method, params);
   }
+    public async register() {
+        return this.callModuleReturn("register");
+    }
 }
 
 export const createClient = factory<EthClient>(EthClient, MODULE);
