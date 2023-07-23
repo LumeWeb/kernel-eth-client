da59c69afdd43785bb6b08568d71217c1da47689
Derrick Hammer <derrick@derrickhammer.com>
2023-07-23 13:39:37 -0400

    refactor: have EthClient extend NetworkClient

--- src/client.ts
+++ src/client.ts
@@ -1,17 +1,9 @@
-import { Client, factory } from "@lumeweb/libkernel/module";
-
+import { factory, NetworkClient } from "@lumeweb/libkernel/module";
 const MODULE = "zduNWtikeTg3FwuNKBNLnWLFJ2ERg4BuDXymerS1gxcbQr5kQ4u2mXtUoq";
 
-export class EthClient extends Client {
-  public async ready() {
-    return this.callModuleReturn("ready");
-  }
+export class EthClient extends NetworkClient {
   public async method(method: string, params: any) {
     return this.callModuleReturn(method, params);
-  }
-
-  public async register() {
-    return this.callModuleReturn("register");
   }
 }
 
