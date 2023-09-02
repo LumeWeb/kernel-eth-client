d6efe228d46f1f95d05dbf5c9a1e6b475cc2a11c
Derrick Hammer <derrick@derrickhammer.com>
2023-09-02 13:50:16 -0400

    dep: update module hash

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { factory, NetworkClient } from "@lumeweb/libkernel/module";
-const MODULE = "zduLxmoG88oCttB4XDWkz5V5zVdEGffo1njvd2Hh6iDGLvxyi4t6ufkd83";
+const MODULE = "z3o47Zqqkx7XD9Td5KU2qsgjWk5hrz7U5eirxcDKvJdyvoaGfzp94DNVm9af";
 
 export class EthClient extends NetworkClient {
   public async method(method: string, params: any) {
